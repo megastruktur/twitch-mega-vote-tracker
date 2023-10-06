e9fc6ddbde4cf9601ab9c10622f48e163ef92605
megastruktur <astromortis@gmail.com>
2023-10-06 16:21:37 +0300

    Adds numbers to listing

--- src/lib/components/GameCard.svelte
+++ src/lib/components/GameCard.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import type { GamesResponse, SteamGameDataResponse } from "$lib/pocketbase-types";
+
   export let game: GamesResponse
 
   let steamGameData: SteamGameDataResponse = game.expand["steam_game_data(game)"][0]
@@ -9,8 +10,13 @@
 
 {#if steamGameData !== undefined}
   <button class="relative game-card-button w-full h-full" data-game-id={game.id}>
+    <div class="px-3 absolute top-0 left-0 bg-slate-700/70">
+      <span class="h3">{game.weight}.</span>
+    </div>
     {#if game.votesTotal !== 0}
-      <div class="pl-3 absolute bottom-0 bg-slate-700/70 w-full">Сумма: {game.votesTotal}</div>
+      <div class="pl-3 absolute bottom-0 bg-slate-700/70 w-full">
+        <span>Сумма: {game.votesTotal}</span>
+      </div>
     {/if}
     {#if steamGameData.header_image}
       <img
--- src/lib/models/games.ts
+++ src/lib/models/games.ts
@@ -2,8 +2,18 @@ import { pb } from "../pocketbase";
 import type { GamesResponse } from "$lib/pocketbase-types";
 
 export async function getAllGames(): Promise<GamesResponse[]>{
-  return await pb.collection('games').getFullList({
+  
+  let games: (GamesResponse & {weight?: number})[] = await pb.collection('games').getFullList({
     sort: '-votesTotal',
     expand: 'steam_game_data(game)',
   });
+
+  let weightCounter = 1
+  games = games.map((game) => {
+    game.weight = weightCounter
+    weightCounter++
+    return game
+  })
+
+  return games
 }
